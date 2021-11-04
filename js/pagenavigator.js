d223ca408c91224a4bcda89c59590363ddafab6d
Дмитрий Иглаков <cjdmitri@gmail.com>
2021-11-04 19:57:54 +0300

    add settings

--- js/pagenavigator.js
+++ js/pagenavigator.js
@@ -1,19 +1,41 @@
+let settings={
+    tags:"h2, h3, h4, h5, h6",
+    pagenavigatorid:"pagenavigator",
+    generateid:true,
+    generatename:"header"
+}
+
 document.addEventListener('DOMContentLoaded', startup());
 
 
 
 function startup() {
-    let pagenavigator = document.getElementById('pagenavigator');
+    console.info(settings.pagenavigatorid);
+    let pagenavigator = document.getElementById(settings.pagenavigatorid);
+    //Проверяем наличие контейнера для оглавления
     if (pagenavigator === null) {
         console.info('No container PageNavigatorJS');
         return;
     }
-    let headersTags = document.querySelectorAll('h2, h3, h4, h5, h6');
+    let headersTags = document.querySelectorAll(settings.tags);
 
+    //если нет заголовков, то выходим
     if(headersTags.length===0){
         console.info('No headers');
         return;
     }
+
+    //Если необходимо генерировать ид
+    if(settings.generateid){
+        headersTags.forEach(function (item, i){
+            if(item.id===""){
+                let id = `${settings.generatename}${i}`;
+                item.id=id;
+                console.log(id);
+            }
+        });
+    }
+    
 
     //необходима для определения вложенности списка
     let lastNumberTag;
